e6a46f68148d3364cb636f46c02213fa4447fbfd
Rafael George <george.rafael@gmail.com>
2011-08-23 10:40:27 -0400

    added kilo's and centimeters calculation

--- ex5.py
+++ ex5.py
@@ -8,7 +8,9 @@ hair = 'Brown'
 
 print "Let's talk about %s." % name
 print "He's %d inches tall." % height
+print "He's %d centimeters tall." %  (height * 2.54)
 print "He's %d pounds heavy." % weight
+print "He's %d kilo's heavy." % (weight / 2.2)
 print "Actually that's not too heavy."
 print "He's got %s eyes and %s hair." % (eyes, hair)
 print "His teeth are usually %s depending on the coffee." % teeth
