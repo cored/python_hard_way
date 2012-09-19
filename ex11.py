5cdcf592286026767ca33d2b12987d5ba5ffd9aa
Rafael George <george.rafael@gmail.com>
2012-09-19 17:50:30 -0400

    Add ex 11 and 12

---
+++ ex11.py
@@ -0,0 +1,8 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How mucho do you weigh?",
+weigh = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % (age, height, weigh)
---
+++ ex12.py
@@ -0,0 +1,6 @@
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+weigh = raw_input("How much do you weigh? ")
+
+print "So, you are %r old, %r tal and %r heavy." % (age, height, weigh)
+
